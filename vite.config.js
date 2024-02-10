b371e505e1e91ca585c386f943ea0e807c3be68b
zhilovhub <104529663+zhilovhub@users.noreply.github.com>
2024-02-10 22:00:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  base: '/vite-deploy-demo/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
