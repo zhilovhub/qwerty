69f5f633bf8177740e5655d2c626ad072fe720fa
Ilya <zhilovgithub@gmail.com>
2024-02-11 08:42:05 +0300

    fixed bug

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
-  base: '/vite-deploy-demo/',
+  base: '/qwerty/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
